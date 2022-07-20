c05db9d893ee43cdd3baf85d5da3e3b206821b2d
Амир Керимов <amir@MacBook-Pro-Amir.local>
2022-07-20 10:33:32 +0300

    fix: Change api base url to correct

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const BEATFILM_BASE_URL = 'https://api.nomoreparties.co';
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://amir.movies-explorer.nomoreparties.sbs/api';
 export const errorMessage = 'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
 export const nameRegex = /^[a-яёa-z -]+$/i
 export const nameInvalidMessage = 'Имя может содержать только кириллицу, латиницу, пробел и дефис';
