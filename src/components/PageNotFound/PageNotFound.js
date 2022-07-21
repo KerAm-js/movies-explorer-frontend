f7587a4f012bc6e4a5f113e0712256e9dc801ad0
Амир Керимов <amir@MacBook-Pro-Amir.local>
2022-07-22 00:24:54 +0300

    fix: Fix bag with 404 page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,15 +1,19 @@
 import React from "react";
-import Link from "../Link/Link";
+import { useNavigate } from "react-router-dom";
+import Button from "../Button/Button";
 import './PageNotFound.css';
 
-function PageNotFound({ history }) {
+function PageNotFound() {
+
+  const navigate = useNavigate()
+
   return (
     <div className="page-not-found">
       <div className="page-not-found__main">
         <h1 className="page-not-found__title">404</h1>
         <p className="page-not-found__subtitle">Страница не найдена</p>
       </div>
-      <Link className="page-not-found__link" title="Назад" to="#" />
+      <Button className="page-not-found__link" title="Назад" onClick={() => navigate('/')} />
     </div>
   )
 }
